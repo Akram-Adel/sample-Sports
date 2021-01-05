e55eb374431057dd761a37d8d4273789c1b99540
Akram-Adel <akrmadel33@gmail.com>
2021-01-05 20:23:39 +0200

    feat: Added Home page

---
+++ src/screens/home/components/banner.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// eslint-disable-next-line arrow-body-style
+const Banner = () => {
+  return (
+    <div className="banner">
+      <h1>
+        Match 23
+      </h1>
+
+      <h3 className="fw-light mt-3">
+        T20 world cup 2020
+      </h3>
+    </div>
+  );
+};
+export default Banner;
--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,11 +1,53 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import moment from 'moment';
 
-// eslint-disable-next-line arrow-body-style
+import { articles } from 'services/newsService';
+
+import Banner from './components/banner';
+
 const Home = () => {
+  // State
+  const [articlesData, setArticlesData] = useState<typeof articles>([]);
+  useEffect(() => {
+    // If this was an API call, a good idea here is to save the results in a store (redux store or similar)
+    // as those results will be used in the all news page
+    setArticlesData(articles.slice(0, 8));
+  }, []);
+
   return (
-    <div>
-      Hello Home
-    </div>
+    <>
+      <Banner />
+
+      <div className="d-flex justify-content-between my-4">
+        <div className="fw-bold">
+          Recentrly Added
+        </div>
+        <button type="button" className="btn btn-light">
+          Show All
+        </button>
+      </div>
+
+      <div className="row">
+        {articlesData.map((article) => (
+          <div key={article.id} className="card col-lg-3 col-sm-4 mb-4 border-0">
+            <div
+              className="card-img-top card-bg-img"
+              style={{ backgroundImage: `url(${article.urlToImage})` }} />
+
+            <div className="card-body">
+              <h5 className="card-title">
+                {article.title.slice(0, 45)}
+                {article.title.length > 45 && '...'}
+              </h5>
+              <small className="card-text text-muted">
+                {moment(article.publishedAt).format('MMM DD/YYYY hh:mm a')}
+              </small>
+            </div>
+          </div>
+
+        ))}
+      </div>
+    </>
   );
 };
 export default Home;
